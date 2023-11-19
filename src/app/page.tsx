bf51f60759e0cee1eb2c6f64448d3ff23a41bf91
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-11-19 15:53:27 +0100

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   };
 
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+    <main className='flex min-h-screen flex-col items-center justify-between p-24 bg-gray-100'>
       <h1>ecommerce store</h1>
     </main>
   );
