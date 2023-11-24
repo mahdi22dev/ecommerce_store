0829c97a362783ac027bb20ab950288654d0cc67
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-11-24 16:32:36 +0100

    c

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,12 @@ import { allProductSchema } from "@/lib/schema/schemaValidation";
 import { allFetch } from "@/server-actions/sanityActions";
 
 export default async function Home() {
-  // const products: allType = await allFetch();
-  // const validProductSchema = allProductSchema.safeParse(products);
+  // try {
+  //   const products: allType = await allFetch();
+  //   const validProductSchema = allProductSchema.safeParse(products);
+  // } catch (error) {
+  //   throw error;
+  // }
 
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24 pb-[200%]'></main>
---
+++ src/components/Home/FirstPartOfPage.tsx
@@ -0,0 +1,8 @@
+import { allType } from "@/lib/sanity/sanityQuerieTypes";
+import React from "react";
+
+function FirstPartOfPage({ data }: { data: allType }) {
+  return <div></div>;
+}
+
+export default FirstPartOfPage;
