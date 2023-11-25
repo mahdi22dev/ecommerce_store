255affb35fa6b381b03f1e6c4006fe77985dbb56
mahdi22dev <129465404+mahdi22dev@users.noreply.github.com>
2023-11-25 03:28:59 +0100

    complete grid

--- src/components/Home/FirstGrid.tsx
+++ src/components/Home/FirstGrid.tsx
@@ -1,15 +1,55 @@
 import { IntrouductionPageGridData } from "@/config/data";
 import { allType } from "@/lib/sanity/sanityQuerieTypes";
+import Image from "next/image";
 import React from "react";
 
 function FirstGrid() {
   const data = IntrouductionPageGridData;
   return (
     <div className='grid grid-cols-1 md:grid-cols-3 gap-3 w-full'>
-      <div className='bg-yellow-200 h-80 md:col-span-2	'>{data[0].category}</div>
-      <div className='bg-yellow-200 h-80'>{data[1].category}</div>
-      <div className='bg-yellow-200 h-80'>{data[2].category}</div>
-      <div className='bg-yellow-200 h-80 md:col-span-2'>{data[3].category}</div>
+      <div className='bg-yellow-200 h-80 md:col-span-2	relative'>
+        <Image
+          src={data[0].image}
+          alt={data[0].category}
+          fill
+          objectFit='cover'
+          className='z-0'
+        />
+        <p className='z-50'>{data[0].category}</p>
+      </div>
+      <div className='bg-yellow-200 h-80'>
+        {" "}
+        <Image
+          src={data[1].image}
+          alt={data[1].category}
+          fill
+          objectFit='cover'
+          className='z-0'
+        />
+        <p className='z-50'>{data[1].category}</p>
+      </div>
+      <div className='bg-yellow-200 h-80'>
+        {" "}
+        <Image
+          src={data[2].image}
+          alt={data[2].category}
+          fill
+          objectFit='cover'
+          className='z-0'
+        />
+        <p className='z-50'>{data[2].category}</p>
+      </div>
+      <div className='bg-yellow-200 h-80 md:col-span-2'>
+        {" "}
+        <Image
+          src={data[3].image}
+          alt={data[3].category}
+          fill
+          objectFit='cover'
+          className='z-0'
+        />
+        <p className='z-50'>{data[3].category}</p>
+      </div>
     </div>
   );
 }
--- src/config/data.ts
+++ src/config/data.ts
@@ -1,6 +1,26 @@
 export const IntrouductionPageGridData = [
-  { id: 1, image: "", category: "FUNITURE" },
-  { id: 2, image: "", category: "LIGHTING" },
-  { id: 3, image: "", category: "Décor & Pillows" },
-  { id: 4, image: "", category: "Rugs" },
+  {
+    id: 1,
+    image:
+      "https://images.pexels.com/photos/1866149/pexels-photo-1866149.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    category: "FUNITURE",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.pexels.com/photos/1176516/pexels-photo-1176516.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    category: "LIGHTING",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.pexels.com/photos/1248583/pexels-photo-1248583.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    category: "Décor & Pillows",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.pexels.com/photos/2747901/pexels-photo-2747901.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    category: "Rugs",
+  },
 ];
